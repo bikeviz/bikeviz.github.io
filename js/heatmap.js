64b1b5f66a92bdb69373daea3bfe358855a66e46
girikuncoro <girikuncoro@gmail.com>
2015-05-14 16:27:59 -0400

    added subheader

---
+++ js/heatmap.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+  draw_heatmap("../result/dc_round_trips.tsv", "#dccommuter");
+  draw_heatmap("../result/dc_one_way.tsv", "#dcleisure");
+  draw_heatmap("../result/nyc_one_way.tsv", "#nyccommuter");
+  draw_heatmap("../result/nyc_round_trips.tsv", "#nycleisure");
+});
+
+var margin = { top: 50, right: 0, bottom: 100, left: 30 },
+    width = 960 - margin.left - margin.right,
+    height = 430 - margin.top - margin.bottom,
+    gridSize = Math.floor(width / 24),
+    legendElementWidth = gridSize*2,
+    buckets = 9,
+    colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+    // colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+    days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+    times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+
+function draw_heatmap(source, div) {
+  d3.tsv(source,
+    function(d) {
+      return {
+        day: +d.day,
+        hour: +d.hour,
+        value: +d.count
+      };
+    },
+    function(error, data) {
+      var maxCount = d3.max(data, function (d) { return d.value; });
+
+      var colorScale = d3.scale.quantile()
+          .domain([0, buckets - 1, 100])
+          .range(colors);
+
+      var svg = d3.select(div).append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var dayLabels = svg.selectAll(".dayLabel")
+          .data(days)
+          .enter().append("text")
+            .text(function (d) { return d; })
+            .attr("x", 0)
+            .attr("y", function (d, i) { return i * gridSize; })
+            .style("text-anchor", "end")
+            .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+            .attr("class", function (d, i) { return ((i >= 1 && i <= 5) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+
+      var timeLabels = svg.selectAll(".timeLabel")
+          .data(times)
+          .enter().append("text")
+            .text(function(d) { return d; })
+            .attr("x", function(d, i) { return i * gridSize; })
+            .attr("y", 0)
+            .style("text-anchor", "middle")
+            .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+            .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+
+      var heatMap = svg.selectAll(".hour")
+          .data(data)
+          .enter().append("rect")
+          // .attr("x", function(d) { return (d.hour - 1) * gridSize; })
+          // .attr("y", function(d) { return (d.day - 1) * gridSize; })
+          .attr("x", function(d) { return (d.hour) * gridSize; })
+          .attr("y", function(d) { return (d.day) * gridSize; })
+          .attr("rx", 4)
+          .attr("ry", 4)
+          .attr("class", "hour bordered")
+          .attr("width", gridSize)
+          .attr("height", gridSize)
+          .style("fill", colors[0]);
+
+      heatMap.transition().duration(1000)
+          .style("fill", function(d) { return colorScale(d.value * 100 / maxCount); });
+
+      heatMap.append("title").text(function(d) { return Math.round(d.value * 100 / maxCount) + " %"; });
+          
+      var legend = svg.selectAll(".legend")
+          .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+          .enter().append("g")
+          .attr("class", "legend");
+
+      legend.append("rect")
+        .attr("x", function(d, i) { return legendElementWidth * i; })
+        .attr("y", height)
+        .attr("width", legendElementWidth)
+        .attr("height", gridSize / 2)
+        .style("fill", function(d, i) { return colors[i]; });
+
+      legend.append("text")
+        .attr("class", "mono")
+        .text(function(d) { return "≥ " + (Math.round(d)) + " %"; })
+        .attr("x", function(d, i) { return legendElementWidth * i; })
+        .attr("y", height + gridSize);
+
+      svg.append("text")
+        .text("percentage of riders")
+        .attr("x", function(d) { return legendElementWidth * 9; })
+        .attr("y", height + gridSize)
+  });
+}
